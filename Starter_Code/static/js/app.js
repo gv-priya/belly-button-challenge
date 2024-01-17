d40105c1de9cce0ea21b79b4256b4e24b99f1e32
gayathripriya <gvijayakumar_1@outlook.com>
2024-01-17 11:29:20 +1100

    updates to bellybutton-challenge

--- Starter_Code/static/js/app.js
+++ Starter_Code/static/js/app.js
@@ -1,3 +1,23 @@
-fetch('.//Starter_Code/samples.json')
-    .then((response) => response.json)
-    .then((data) => console.log(json));
+n =[];
+let option;
+var dropdown = d3.select("selDataset");
+function init(){
+d3.json("https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json").then(function(data){
+    console.log(data);
+    var names = data.samples.map(x=>x.id);     
+    var otu_ids = data.samples.map(x=>x.otu_ids);
+    var otu_labels = data.samples.map(y=>y.otu_labels);
+    var sample_values = data.samples.map(z=>z.sample_values)     
+    names.forEach(function(name){
+        d3.select("#selDataset")
+        .append('option')
+        .text(name)
+    });
+    
+
+}); 
+};
+init();
+/* first 10 OTU found 1individual sorted by asc or desc*/
+
+/*sample metadata in demographic data */
